6c2fbbb5f91eb1a63f5a8603bec3492dfd2c6fe0
Oleg Mishunin <rjawiygvozd@gmail.com>
2025-10-27 15:51:05 +0700

    another workaround for keyword used as column name

--- libtsql2psql/TSqlToPSqlConverter/NodeExtensions.cs
+++ libtsql2psql/TSqlToPSqlConverter/NodeExtensions.cs
@@ -179,7 +179,8 @@ public static class ConverterNodeExtensions {
             || node.Name == SqlStructureConstants.ENAME_BRACKET_QUOTED_NAME 
             || node.TextValue?.ToLower() == "value"
             || node.TextValue?.ToLower() == "text"
-            || node.TextValue?.ToLower() == "status";
+            || node.TextValue?.ToLower() == "status"
+            || node.TextValue?.ToLower() == "str";
     }
 
     public static Node FollowingNonWsChild(this Node value, Node fromChild, bool allowComments = false)
