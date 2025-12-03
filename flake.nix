e333733452e666302b0613d1a128ebbc5fa7ad16
Oleg Mishunin <rjawiygvozd@gmail.com>
2025-12-03 11:29:54 +0700

    flake bump

--- flake.nix
+++ flake.nix
@@ -2,41 +2,50 @@
   description = "tsql2psql";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-  flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        config = { allowUnfree = true; };
-      };
-
-      dotnet-sdk = pkgs.buildEnv {
-        name = "dotnet-sdk";
-        paths = [
-          (with pkgs.dotnetCorePackages;
-            combinePackages [
-              sdk_9_0
-              sdk_8_0
-            ]
-          )
-        ];
-      };
-    in
-    with pkgs;
+  outputs =
     {
-      devShells.default = mkShell {
-        buildInputs = [
-          netcoredbg
-          dotnet-sdk
-          omnisharp-roslyn 
-        ];
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
 
-        DOTNET_ROOT = "${dotnet-sdk}/share/dotnet";
-      };
-    }
-  );
+        dotnet-sdk = pkgs.buildEnv {
+          name = "dotnet-sdk";
+          paths = [
+            (
+              with pkgs.dotnetCorePackages;
+              combinePackages [
+                sdk_9_0
+                sdk_8_0
+              ]
+            )
+          ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            netcoredbg
+            dotnet-sdk
+            omnisharp-roslyn
+          ];
+
+          DOTNET_ROOT = "${dotnet-sdk}/share/dotnet";
+        };
+      }
+    );
 }
