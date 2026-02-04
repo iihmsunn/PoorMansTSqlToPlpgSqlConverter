ed54f55d012323ed1f74a815c42c17cb64af41d6
Oleg Mishunin <rjawiygvozd@gmail.com>
2026-02-04 19:42:58 +0700

    boolean <> int operator

--- sql_functions/boolean_compare_operator.sql
+++ sql_functions/boolean_compare_operator.sql
@@ -21,3 +21,27 @@ CREATE OPERATOR = (
     procedure = op_eq_boolean_int,
     commutator = =
 );
+
+DROP OPERATOR IF EXISTS <> (boolean, int);
+DROP FUNCTION IF EXISTS op_non_eq_boolean_int;
+
+CREATE FUNCTION op_non_eq_boolean_int(arg1 boolean, arg2 int)
+RETURNS boolean AS $$
+BEGIN
+    RETURN CASE
+    WHEN arg1 and arg2 = 0
+    THEN true
+    WHEN not arg1 and arg2 = 1
+    THEN true
+    ELSE false
+    END;
+END; $$
+STABLE
+LANGUAGE plpgsql;
+
+CREATE OPERATOR <> (
+    leftarg = boolean,
+    rightarg = int,
+    procedure = op_non_eq_boolean_int,
+    commutator = <>
+);
