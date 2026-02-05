766da75fbd8394199d9719fc014e5e8a60bedc87
Oleg Mishunin <rjawiygvozd@gmail.com>
2026-02-05 09:54:14 +0700

    alias string_split as split

--- sql_functions/string_split.sql
+++ sql_functions/string_split.sql
@@ -3,4 +3,11 @@ returns setof text
 language 'sql' immutable
 as $$
   select unnest(string_to_array(string, separator))
+$$;
+
+create or replace function split(string text, separator text)
+returns setof text
+language 'sql' immutable
+as $$
+  select unnest(string_to_array(string, separator))
 $$
