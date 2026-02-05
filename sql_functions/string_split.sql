a6ad2d504cf39e77a4d33a14a958cd80b1e96072
Oleg Mishunin <rjawiygvozd@gmail.com>
2026-02-05 10:15:29 +0700

    make split have compatible interface

--- sql_functions/string_split.sql
+++ sql_functions/string_split.sql
@@ -2,12 +2,12 @@ create or replace function string_split(string text, separator text)
 returns setof text
 language 'sql' immutable
 as $$
-  select unnest(string_to_array(string, separator))
+  select unnest(string_to_array(string, separator)) as value
 $$;
 
 create or replace function split(string text, separator text)
-returns setof text
+returns table (value text)
 language 'sql' immutable
 as $$
-  select unnest(string_to_array(string, separator))
+  select unnest(string_to_array(string, separator)) as value
 $$
