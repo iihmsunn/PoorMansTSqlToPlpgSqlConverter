b679f9f6821c21892bf19e51b0744c77c97504ab
Oleg Mishunin <rjawiygvozd@gmail.com>
2026-02-12 17:54:23 +0700

    crash fix

--- libtsql2psql/TSqlToPSqlConverter/SyntaxTreeTransformer.cs
+++ libtsql2psql/TSqlToPSqlConverter/SyntaxTreeTransformer.cs
@@ -2307,7 +2307,7 @@ public class SyntaxTreeTransformer {
         if (element.Matches(SqlStructureConstants.ENAME_FUNCTION_KEYWORD, "datepart")) {
             element.TextValue = "extract";
             var parens = element.NextNonWsSibling();
-            var interval = parens.ChildByNameAndText(SqlStructureConstants.ENAME_OTHERNODE)!;
+            var interval = parens.Children.First(e => e.Name == SqlStructureConstants.ENAME_OTHERNODE || e.Name == SqlStructureConstants.ENAME_FUNCTION_KEYWORD)!;
             if (DateMapping.ContainsKey(interval.TextValue.ToLower()))
             {
                 interval.TextValue = DateMapping[interval.TextValue.ToLower()];
